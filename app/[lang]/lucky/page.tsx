ff4a44d4fd0ef88c207b36686ab71741b9b416d3
Khai Fahmi Zaki <khaifahmi99@gmail.com>
2025-01-18 17:29:46 +1100

    fix: make the /lucky redirect to be client component
    server component that is prerendered cannot use Math.random as it is non-deterministic

--- app/[lang]/lucky/page.tsx
+++ app/[lang]/lucky/page.tsx
@@ -1,12 +1,15 @@
-import { redirect } from "next/navigation";
+'use client';
 
-export default async function Page() {
+import { useRouter } from "next/navigation";
+
+export default function Page() {
+  const router = useRouter();
+
   const maxEmoticons = 60_000; // FIXME: This should be based on the db row counts
-
   const random = Math.floor(Math.random() * maxEmoticons) + 1;
 
   // 8-digit id format (padded with zeros)
   const formatted = random.toString().padStart(8, '0');
 
-  return redirect(`/emoticons/${formatted}`);
+  router.replace(`/emoticons/${formatted}`)
 }
